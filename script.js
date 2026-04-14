cd127c65d698d98d08cd117c1a505c45242e24cb
Shreyaas <shreyaasgupta280@gmail.com>
2026-04-14 15:35:47 +0530

    updated game logic and UI

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const choices=["rock","paper","scissors"];
 const playerchoice=document.getElementById('playerDisplay');
 const computerchoice=document.getElementById('computerDisplay');
 const result=document.getElementById('resultDisplay');
+const playerscore=document.getElementById('playerScore');
+const computerscore=document.getElementById('computerScore');
+let playerScore=0;
+let computerScore=0;
 
 function playGames(playerChoice){
     const computerChoice=choices[Math.floor(Math.random()*3)];
@@ -26,4 +30,16 @@ function playGames(playerChoice){
     playerchoice.textContent=`PLAYER: ${playerChoice}`;
     computerchoice.textContent=`COMPUTER: ${computerChoice}`;
     result.textContent=resultText;
+
+    resultDisplay.classList.remove("greenText", "redText");
+    if(resultText==="You win!"){
+        playerScore++;
+        resultDisplay.classList.add("greenText");
+    } else if(resultText==="You lose!"){
+        computerScore++;
+        resultDisplay.classList.add("redText");
+    }
+    playerscore.textContent=playerScore;
+    computerscore.textContent=computerScore;
+
 }
